2037694898135fde5ec43050a3694d36ac4fc326
Fernando Via Canel <fernando.via@gmail.com>
2019-06-06 16:46:32 +0200

    🐣 fantasy-color/from-hex: move package from fantasy-color

---
+++ packages/fantasy-color/from-hex/src/index.ts
@@ -0,0 +1,13 @@
+import { RGB } from '@fantasy-color/types'
+
+export default (hex: string): RGB | null => {
+  if (hex[0] !== '#' || hex.length !== 7) {
+    return null
+  }
+
+  return {
+    red: parseInt(hex.slice(1, 3), 16),
+    green: parseInt(hex.slice(3, 5), 16),
+    blue: parseInt(hex.slice(5, 7), 16),
+  }
+}
