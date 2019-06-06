9b078c7d68cbc97c7682fff595108de7a68900df
Fernando Via Canel <fernando.via@gmail.com>
2019-06-06 16:48:03 +0200

    🐣 fantasy-color/from-rgb: move package from fantasy-color

---
+++ packages/fantasy-color/from-rgb/src/index.ts
@@ -0,0 +1,16 @@
+import { RGB } from '@fantasy-color/types'
+
+const rgbRegex = /^\s*rgb\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)\s*$/
+
+export default (rgb: string): RGB | null => {
+  const matches = rgb.match(rgbRegex)
+  if (matches === null) {
+    return null
+  }
+
+  return {
+    red: parseInt(matches[1]),
+    green: parseInt(matches[2]),
+    blue: parseInt(matches[3]),
+  }
+}
