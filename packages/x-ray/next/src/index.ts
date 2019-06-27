8794ccbb5fed76ef0c72509d2ccc3faf99ec1fc8
Kir Belevich <kir@belevi.ch>
2019-06-27 15:26:03 +0200

    🚧 x-ray/next

---
+++ packages/x-ray/next/src/index.ts
@@ -0,0 +1,92 @@
+import path from 'path'
+import { createReadStream, createWriteStream } from 'graceful-fs'
+import tarStream from 'tar-stream'
+
+export type TTarFs = {
+  list: () => Iterable<string>,
+  read: (filePath: string) => Buffer,
+  write: (filePath: string, data: Buffer) => void,
+  close: () => Promise<void>,
+}
+
+export const TarFs = (tarFilePath: string) => new Promise<TTarFs>((resolve, reject) => {
+  const extract = tarStream.extract()
+  const files = new Map<string, Buffer>()
+  let hasChanged = false
+
+  const api: TTarFs = {
+    list: () => files.keys(),
+    read: (filePath) => {
+      if (!files.has(filePath)) {
+        throw new Error(`File ${filePath} doesn't exist`)
+      }
+
+      return files.get(filePath)!
+    },
+    write: (filePath, data) => {
+      hasChanged = true
+
+      files.set(filePath, data)
+    },
+    close: () => new Promise((writeResolve, writeReject) => {
+      if (!hasChanged) {
+        return writeResolve()
+      }
+
+      const writeStream = createWriteStream(tarFilePath)
+      const pack = tarStream.pack()
+
+      for (const [name, data] of files.entries()) {
+        pack.entry({ name }, data)
+      }
+
+      pack.on('error', writeReject)
+      writeStream.on('error', writeReject)
+      writeStream.on('finish', writeResolve)
+
+      pack.pipe(writeStream)
+    }),
+  }
+
+  extract.on('entry', (header, stream, next) => {
+    let data = Buffer.from([])
+
+    stream.on('data', (chunk) => {
+      data = Buffer.concat([data, chunk])
+    })
+
+    stream.on('end', () => {
+      if (header.type === 'file') {
+        files.set(header.name, data)
+      }
+
+      next()
+    })
+  })
+
+  extract.on('finish', () => resolve(api))
+
+  extract.on('error', reject)
+
+  createReadStream(tarFilePath)
+    .on('error', (error) => {
+      if (error.code === 'ENOENT') {
+        resolve(api)
+      } else {
+        reject(error)
+      }
+    })
+    .pipe(extract)
+})
+
+;(async () => {
+  const tarFs = await TarFs(path.resolve('packages/x-ray/next/src/test3.tar'))
+
+  // tarFs.write('file-3.txt', Buffer.from('FOO!'))
+
+  console.log(tarFs.list())
+
+  console.log(tarFs.read('file-3.txt').toString())
+
+  // await tarFs.close()
+})()
