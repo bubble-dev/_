fa4251e37be9eef9d9f1504b94b5a27b8ad100a2
Fernando Via Canel <fernando.via@gmail.com>
2019-07-05 10:37:23 +0200

    🚧

---
+++ packages/primitives/button/src/Button.email.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { normalizeStyle } from 'stili'
+import { component, startWithType, mapWithPropsMemo } from 'refun'
+import { TButtonProps } from './types'
+
+export const Button = component(
+  startWithType<TButtonProps>(),
+  mapWithPropsMemo(({ isDisabled }) => ({
+    styles: normalizeStyle({
+      appearance: 'none',
+      background: 'none',
+      border: 0,
+      cursor: isDisabled ? 'auto' : 'pointer',
+      boxSizing: 'border-box',
+      display: 'flex',
+      flexDirection: 'row',
+      position: 'relative',
+      alignSelf: 'stretch',
+      flexGrow: 1,
+      flexShrink: 1,
+      minWidth: 0,
+      margin: 0,
+      outline: 0,
+      padding: 0,
+      tapHighlightColor: 'rgba(255, 255, 255, 0)',
+      userSelect: 'none',
+    }),
+  }), ['isDisabled'])
+)(({
+  id,
+  accessibilityLabel,
+  isDisabled,
+  styles,
+  onPointerEnter,
+  onPointerLeave,
+  onPress,
+  onPressIn,
+  onPressOut,
+  onFocus,
+  onBlur,
+  children,
+}) => (
+  <a
+    aria-label={accessibilityLabel}
+    disabled={isDisabled}
+    id={id}
+    onClick={onPress}
+    onMouseEnter={onPointerEnter}
+    onMouseLeave={onPointerLeave}
+    onMouseDown={onPressIn}
+    onMouseUp={onPressOut}
+    onFocus={onFocus}
+    onBlur={onBlur}
+    style={styles}
+  >
+    {children}
+  </a>
+))
+
+Button.displayName = 'Button'
---
+++ packages/primitives/layout/src/OutOfFlow.email.tsx
@@ -0,0 +1,8 @@
+import React, { Fragment, FC } from 'react'
+import { TLayoutOutOfFlow } from './types'
+
+export const LayoutOutOfFlow: FC<TLayoutOutOfFlow> = () => (
+  <Fragment/>
+)
+
+LayoutOutOfFlow.displayName = 'LayoutOutOfFlow'
