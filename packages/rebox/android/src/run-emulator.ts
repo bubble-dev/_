7f1e632d4be9c2d4baedff9dfd3788003add0ea4
Kir Belevich <kir@belevi.ch>
2019-07-09 17:30:23 +0200

    🛠 check if rebox AVD already exists

--- packages/rebox/android/src/run-emulator.ts
+++ packages/rebox/android/src/run-emulator.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import { homedir } from 'os'
-import { writeFile } from 'pifs'
+import { access, writeFile } from 'pifs'
 import execa from 'execa'
 import { isString } from 'tsfn'
 import makeDir from 'make-dir'
@@ -25,45 +25,49 @@ export const runEmulator = async (options: TRunEmulator): Promise<() => void> =>
   const configIniPath = path.join(reboxAvdPath, 'config.ini')
   const reboxIniPath = path.join(avdHomePath, 'rebox.ini')
 
-  await makeDir(reboxAvdPath)
+  try {
+    await access(reboxAvdPath)
+  } catch {
+    await makeDir(reboxAvdPath)
 
-  const reboxIniData = [
-    'avd.ini.encoding=UTF-8',
-    `path=${reboxAvdPath}`,
-    'path.rel=avd/rebox.avd',
-    'target=android-28',
-  ]
+    const reboxIniData = [
+      'avd.ini.encoding=UTF-8',
+      `path=${reboxAvdPath}`,
+      'path.rel=avd/rebox.avd',
+      'target=android-28',
+    ]
 
-  await writeFile(reboxIniPath, reboxIniData)
+    await writeFile(reboxIniPath, reboxIniData)
 
-  const configIniData = [
-    'PlayStore.enabled=false',
-    'abi.type=x86',
-    'avd.ini.encoding=UTF-8',
-    'hw.accelerometer=yes',
-    'hw.audioInput=no',
-    'hw.audioOutput=no',
-    'hw.battery=yes',
-    'hw.cpu.arch=x86',
-    'hw.dPad=no',
-    'hw.device.manufacturer=Google',
-    'hw.device.name=Nexus 5',
-    'hw.gps=no',
-    'hw.lcd.density=480',
-    'hw.lcd.height=1920',
-    'hw.lcd.width=1080',
-    'hw.mainKeys=no',
-    'hw.sdCard=no',
-    'hw.sensors.orientation=yes',
-    'hw.sensors.proximity=yes',
-    'hw.trackBall=no',
-    'hw.keyboard=yes',
-    'image.sysdir.1=system-images/android-28/google_apis/x86/',
-    'tag.display=Google APIs',
-    'tag.id=google_apis',
-  ].join('\n')
+    const configIniData = [
+      'PlayStore.enabled=false',
+      'abi.type=x86',
+      'avd.ini.encoding=UTF-8',
+      'hw.accelerometer=yes',
+      'hw.audioInput=no',
+      'hw.audioOutput=no',
+      'hw.battery=yes',
+      'hw.cpu.arch=x86',
+      'hw.dPad=no',
+      'hw.device.manufacturer=Google',
+      'hw.device.name=Nexus 5',
+      'hw.gps=no',
+      'hw.lcd.density=480',
+      'hw.lcd.height=1920',
+      'hw.lcd.width=1080',
+      'hw.mainKeys=no',
+      'hw.sdCard=no',
+      'hw.sensors.orientation=yes',
+      'hw.sensors.proximity=yes',
+      'hw.trackBall=no',
+      'hw.keyboard=yes',
+      'image.sysdir.1=system-images/android-28/google_apis/x86/',
+      'tag.display=Google APIs',
+      'tag.id=google_apis',
+    ].join('\n')
 
-  await writeFile(configIniPath, configIniData)
+    await writeFile(configIniPath, configIniData)
+  }
 
   const emulatorProcess = execa(
     `${process.env.ANDROID_HOME}/emulator/emulator`,
