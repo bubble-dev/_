c4a79aa0fc80fa9b1857ea321777ff79c5519a14
alex.feinstein <alex.feinstein@klarna.com>
2019-07-09 15:03:42 +0200

    🛠 remove `files` file

--- packages/x-ray/native-screenshots-app/src/files.js
+++
@@ -1,2 +0,0 @@
-import file0 from '/Users/kir.belevich/Projects/bubble-dev/packages/primitives/button/test/screenshots.tsx'
-export default [{ path: '/Users/kir.belevich/Projects/bubble-dev/packages/primitives/button/test/screenshots.tsx', content: file0 }]
