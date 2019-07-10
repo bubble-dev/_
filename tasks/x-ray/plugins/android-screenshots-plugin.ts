024d0164d4852cae428a7d6448fd30935f8d68e4
Kir Belevich <kir@belevi.ch>
2019-07-10 10:18:52 +0200

    🛠 fix deps

--- tasks/x-ray/plugins/android-screenshots-plugin.ts
+++ tasks/x-ray/plugins/android-screenshots-plugin.ts
@@ -1,85 +1,86 @@
 import plugin, { StartFilesProps } from '@start/plugin'
 
-export default (appPath: string) => plugin<StartFilesProps, void>('x-ray-android-screenshots', ({ logMessage }) => async ({ files }) => {
-  const path = await import('path')
-  const { createServer } = await import('http')
-  const { createReadStream } = await import('fs')
-  const { default: execa } = await import('execa')
-  const { rnResolve } = await import('rn-resolve')
-  const { buildJsBundle, runEmulator } = await import('@rebox/android')
-  const { runServer, prepareFiles } = await import('@x-ray/native-screenshots')
+export default (appPath: string) =>
+  plugin<StartFilesProps, void>('x-ray-android-screenshots', ({ logMessage }) => async ({ files }) => {
+    const path = await import('path')
+    const { createServer } = await import('http')
+    const { createReadStream } = await import('fs')
+    const { default: execa } = await import('execa')
+    const { rnResolve } = await import('rn-resolve')
+    const { buildJsBundle, runEmulator } = await import('@rebox/android')
+    const { runServer, prepareFiles } = await import('@x-ray/native-screenshots')
 
-  const entryPointPath = await rnResolve('@x-ray/native-screenshots-app')
+    const entryPointPath = await rnResolve('@x-ray/native-screenshots-app')
 
-  await prepareFiles(entryPointPath, files.map((file) => file.path))
+    await prepareFiles(entryPointPath, files.map((file) => file.path))
 
-  const bundlePath = await buildJsBundle({
-    entryPointPath,
-    outputPath: path.dirname(appPath),
-  })
+    const bundlePath = await buildJsBundle({
+      entryPointPath,
+      outputPath: path.dirname(appPath),
+    })
 
-  logMessage('bundle is ready')
+    logMessage('bundle is ready')
 
-  const httpServer = createServer((req, res) => {
-    if (req.method === 'GET') {
-      if (req.url === '/status') {
-        res.writeHead(200)
+    const httpServer = createServer((req, res) => {
+      if (req.method === 'GET') {
+        if (req.url === '/status') {
+          res.writeHead(200)
 
-        return res.end('packager-status:running')
+          return res.end('packager-status:running')
+        }
+
+        if (req.url === '/message') {
+          res.writeHead(200)
+
+          return res.end()
+        }
+
+        res.writeHead(200, { 'Content-Type': 'application/javascript' })
+
+        const fileStream = createReadStream(bundlePath)
+
+        fileStream
+          .on('error', (e) => {
+            throw e
+          })
+          .pipe(res)
+      }
+    })
+      .on('error', (e) => {
+        throw e
+      })
+      .listen(8081, '127.0.0.1')
+
+    let killEmulator = null
+
+    try {
+      killEmulator = await runEmulator({
+        isHeadless: false,
+        portsToForward: [3002, 8081],
+      })
+
+      logMessage('device is ready')
+
+      const runScreenshots = await runServer({ platform: 'android' })
+
+      await execa('adb', ['install', '-r', appPath], {
+        stderr: process.stderr,
+      })
+
+      logMessage('x-ray app is installed')
+
+      await execa('adb', ['shell', 'am', 'start', '-n', 'org.bubble_dev.xray/com.rebox.MainActivity'], {
+        stderr: process.stderr,
+      })
+
+      logMessage('x-ray app is launching')
+
+      await runScreenshots()
+    } finally {
+      if (killEmulator !== null) {
+        killEmulator()
       }
 
-      if (req.url === '/message') {
-        res.writeHead(200)
-
-        return res.end()
-      }
-
-      res.writeHead(200, { 'Content-Type': 'application/javascript' })
-
-      const fileStream = createReadStream(bundlePath)
-
-      fileStream
-        .on('error', (e) => {
-          throw e
-        })
-        .pipe(res)
+      httpServer.close()
     }
   })
-    .on('error', (e) => {
-      throw e
-    })
-    .listen(8081, '127.0.0.1')
-
-  let killEmulator = null
-
-  try {
-    killEmulator = await runEmulator({
-      isHeadless: false,
-      portsToForward: [3002, 8081],
-    })
-
-    logMessage('device is ready')
-
-    const runScreenshots = await runServer({ platform: 'android' })
-
-    await execa('adb', ['install', '-r', appPath], {
-      stderr: process.stderr,
-    })
-
-    logMessage('x-ray app is installed')
-
-    await execa('adb', ['shell', 'am', 'start', '-n', 'org.bubble_dev.xray/com.rebox.MainActivity'], {
-      stderr: process.stderr,
-    })
-
-    logMessage('x-ray app is launching')
-
-    await runScreenshots()
-  } finally {
-    if (killEmulator !== null) {
-      killEmulator()
-    }
-
-    httpServer.close()
-  }
-})
--- tasks/x-ray/plugins/ios-screenshots-plugin.ts
+++ tasks/x-ray/plugins/ios-screenshots-plugin.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/named */
 import plugin, { StartFilesProps } from '@start/plugin'
 
 type TDeviceList = {
@@ -31,79 +30,80 @@ const getDeviceInfo = async () => {
   }
 }
 
-export default (appPath: string) => plugin<StartFilesProps, void>('x-ray-ios-screenshots', ({ logMessage }) => async ({ files }) => {
-  if (process.platform !== 'darwin') {
-    return logMessage('BUY A MAC')
-  }
-
-  if (files.length === 0) {
-    return logMessage('no files, skipping')
-  }
-
-  const { createServer } = await import('http')
-  const { createReadStream } = await import('fs')
-  const { default: execa } = await import('execa')
-  const { rnResolve } = await import('rn-resolve')
-  const { buildJsBundle } = await import('@rebox/ios')
-  const { runServer, prepareFiles } = await import('@x-ray/native-screenshots')
-
-  const deviceInfo = await getDeviceInfo()
-
-  if (deviceInfo === null) {
-    throw new Error('Unable to find iOS 11.3 + iPhone 7 Simulator')
-  }
-
-  logMessage(`device id ${deviceInfo.udid}`)
-
-  const entryPointPath = await rnResolve('@x-ray/native-screenshots-app')
-
-  await prepareFiles(entryPointPath, files.map((file) => file.path))
-
-  const bundlePath = await buildJsBundle({
-    entryPointPath,
-    outputPath: appPath,
-  })
-
-  logMessage('x-ray bundle is ready')
-
-  const httpServer = createServer((req, res) => {
-    if (req.method === 'GET') {
-      res.writeHead(200, { 'Content-Type': 'application/javascript' })
-
-      const fileStream = createReadStream(bundlePath)
-
-      fileStream
-        .on('error', (e) => {
-          throw e
-        })
-        .pipe(res)
+export default (appPath: string) =>
+  plugin<StartFilesProps, void>('x-ray-ios-screenshots', ({ logMessage }) => async ({ files }) => {
+    if (process.platform !== 'darwin') {
+      return logMessage('BUY A MAC')
     }
-  })
-    .on('error', (e) => {
-      throw e
-    })
-    .listen(8081, '127.0.0.1')
 
-  try {
+    if (files.length === 0) {
+      return logMessage('no files, skipping')
+    }
+
+    const { createServer } = await import('http')
+    const { createReadStream } = await import('fs')
+    const { default: execa } = await import('execa')
+    const { rnResolve } = await import('rn-resolve')
+    const { buildJsBundle } = await import('@rebox/ios')
+    const { runServer, prepareFiles } = await import('@x-ray/native-screenshots')
+
+    const deviceInfo = await getDeviceInfo()
+
+    if (deviceInfo === null) {
+      throw new Error('Unable to find iOS 11.3 + iPhone 7 Simulator')
+    }
+
+    logMessage(`device id ${deviceInfo.udid}`)
+
+    const entryPointPath = await rnResolve('@x-ray/native-screenshots-app')
+
+    await prepareFiles(entryPointPath, files.map((file) => file.path))
+
+    const bundlePath = await buildJsBundle({
+      entryPointPath,
+      outputPath: appPath,
+    })
+
+    logMessage('x-ray bundle is ready')
+
+    const httpServer = createServer((req, res) => {
+      if (req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/javascript' })
+
+        const fileStream = createReadStream(bundlePath)
+
+        fileStream
+          .on('error', (e) => {
+            throw e
+          })
+          .pipe(res)
+      }
+    })
+      .on('error', (e) => {
+        throw e
+      })
+      .listen(8081, '127.0.0.1')
+
+    try {
     // if (deviceInfo.state !== 'Booted') {
     //   await execa('xcrun', ['simctl', 'boot', deviceInfo.udid])
     // }
 
-    logMessage('device is ready')
+      logMessage('device is ready')
 
-    await execa('xcrun', ['simctl', 'install', 'booted', appPath])
+      await execa('xcrun', ['simctl', 'install', 'booted', appPath])
 
-    logMessage('x-ray app is installed')
+      logMessage('x-ray app is installed')
 
-    const runScreenshots = await runServer({ platform: 'ios' })
+      const runScreenshots = await runServer({ platform: 'ios' })
 
-    await execa('xcrun', ['simctl', 'launch', 'booted', 'org.bubble-dev.xray'])
+      await execa('xcrun', ['simctl', 'launch', 'booted', 'org.bubble-dev.xray'])
 
-    logMessage('x-ray app is launching')
+      logMessage('x-ray app is launching')
 
-    await runScreenshots()
-  } finally {
+      await runScreenshots()
+    } finally {
     // await execa('xcrun', ['simctl', 'shutdown', deviceInfo.udid])
-    httpServer.close()
-  }
-})
+      httpServer.close()
+    }
+  })
