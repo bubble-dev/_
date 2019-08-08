d74cfce4112a14db6773ad45850935c472d0cdbf
alex.feinstein <alex.feinstein@klarna.com>
2019-08-08 12:56:48 +0200

    🐞 autoprops: fix iterable type

--- packages/autoprops/src/get-props-iterable.ts
+++ packages/autoprops/src/get-props-iterable.ts
@@ -3,11 +3,11 @@ import { getPropsImpl } from './get-props'
 import { getNextPermImpl } from './get-next-perm'
 import { TMetaFile } from './types'
 
-export const getPropsIterable = (metaFile: TMetaFile) => ({
+export const getPropsIterable = <T extends {}>(metaFile: TMetaFile) => ({
   int: BigInt.zero as BigInteger | null,
   *[Symbol.iterator]() {
     while (this.int !== null) {
-      yield getPropsImpl(this.int, metaFile)
+      yield getPropsImpl(this.int, metaFile) as T
 
       this.int = getNextPermImpl(this.int, metaFile)
     }
