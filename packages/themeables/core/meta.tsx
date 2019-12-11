7f00977093cbb40ac24fd866379458695708f259
Fernando Via Canel <fernando.via@gmail.com>
2019-12-11 14:07:37 +0100

    🚧 validate that it also works with symbols

--- packages/themeables/core/meta.tsx
+++ packages/themeables/core/meta.tsx
@@ -7,12 +7,14 @@ import { setupTheme, TThemeables } from './src'
 
 type TDemo = { status: 'default' | 'error' }
 
+const SYMBOL_DEMO = Symbol('Demo')
+
 type Mappings = {
-  demo: TDemo,
+  [SYMBOL_DEMO]: TDemo,
 }
 
 const defaultTheme: TThemeables<TThemeableBackground, Mappings> = {
-  demo: ({ status }) => ({
+  [SYMBOL_DEMO]: ({ status }) => ({
     color: status === 'default' ? [0xF0, 0xF0, 0xF0, 1] : [0xFF, 0x99, 0x99, 1],
     bottomLeftRadius: 10,
     bottomRightRadius: 10,
@@ -23,15 +25,15 @@ const defaultTheme: TThemeables<TThemeableBackground, Mappings> = {
 
 const { ThemePiece, createThemeable } = setupTheme<TThemeableBackground, Mappings>(defaultTheme)
 
-export const DemoThemeableBackground = createThemeable<TBackground>('demo', Background)
+export const DemoThemeableBackground = createThemeable<TBackground>(SYMBOL_DEMO, Background)
 
 const newTheme: TThemeables<TThemeableBackground, Mappings> = {
-  demo: ({ status }) => ({
+  [SYMBOL_DEMO]: ({ status }) => ({
     color: status === 'default' ? [0x00, 0x00, 0x00, 1] : [0xFF, 0x00, 0x00, 1],
-    bottomLeftRadius: 10,
-    bottomRightRadius: 10,
-    topLeftRadius: 10,
-    topRightRadius: 10,
+    bottomLeftRadius: 5,
+    bottomRightRadius: 5,
+    topLeftRadius: 5,
+    topRightRadius: 5,
   }),
 }
 
