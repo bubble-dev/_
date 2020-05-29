601cbb53d93580785f9310d83c3a10de631c72eb
irmina.kaczmarek <irmina.kaczmarek@klarna.com>
2020-05-29 18:26:34 +0200

    🐞 primitives/spacer: add missing dependencies

--- packages/primitives/spacer/meta.tsx
+++ packages/primitives/spacer/meta.tsx
@@ -29,7 +29,7 @@ export const Component = (props: TSpacer) => (
           inlineEnd
         </LayoutInFlow>
       </Layout>
-      </LayoutInFlow>
+    </LayoutInFlow>
     <LayoutInFlow>
       blockEnd
     </LayoutInFlow>
