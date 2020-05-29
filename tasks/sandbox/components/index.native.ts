667f8ef9086d7c01558726ed0bc40ef25cc9fed0
irmina.kaczmarek <irmina.kaczmarek@klarna.com>
2020-05-29 18:04:30 +0200

    🐞 primitives/spacer: remove position absolute from spacer

--- tasks/sandbox/components/index.native.ts
+++ tasks/sandbox/components/index.native.ts
@@ -2,22 +2,24 @@ import { TComponents } from '@sandbox/ui'
 /* eslint-disable import/no-extraneous-dependencies */
 import * as Button from '@primitives/button/meta'
 import * as Checkbox from '@primitives/checkbox/meta'
-import * as Input from '@primitives/input/meta'
 import * as Heading from '@primitives/heading/meta'
+import * as Input from '@primitives/input/meta'
+import * as LayoutThemeable from '@themeables/layout/meta'
 import * as Paragraph from '@primitives/paragraph/meta'
 import * as Radio from '@primitives/radio/meta'
-import * as LayoutThemeable from '@themeables/layout/meta'
+import * as Spacer from '@primitives/spacer/meta'
 import * as TestLottie from 'test-lottie/meta'
 import * as VectorShape from '@primitives/vector-shape/meta'
 
 export const components: TComponents = {
   Button: () => Promise.resolve(Button),
   Checkbox: () => Promise.resolve(Checkbox),
-  Input: () => Promise.resolve(Input),
   Heading: () => Promise.resolve(Heading),
+  Input: () => Promise.resolve(Input),
+  LayoutThemeable: () => Promise.resolve(LayoutThemeable),
   Paragraph: () => Promise.resolve(Paragraph),
   Radio: () => Promise.resolve(Radio),
-  LayoutThemeable: () => Promise.resolve(LayoutThemeable),
+  Spacer: () => Promise.resolve(Spacer),
   TestLottie: () => Promise.resolve(TestLottie),
   VectorShape: () => Promise.resolve(VectorShape),
 }
--- tasks/sandbox/components/index.ts
+++ tasks/sandbox/components/index.ts
@@ -3,11 +3,12 @@ import { TComponents } from '@sandbox/ui'
 export const components: TComponents = {
   Button: () => import('@primitives/button/meta' /* webpackChunkName: "Button" */),
   Checkbox: () => import('@primitives/checkbox/meta' /* webpackChunkName: "Checkbox" */),
-  Input: () => import('@primitives/input/meta' /* webpackChunkName: "Input" */),
   Heading: () => import('@primitives/heading/meta' /* webpackChunkName: "Heading" */),
+  Input: () => import('@primitives/input/meta' /* webpackChunkName: "Input" */),
+  LayoutThemeable: () => import('@themeables/layout/meta' /* webpackChunkName: "LayoutThemeable" */),
   Paragraph: () => import('@primitives/paragraph/meta' /* webpackChunkName: "Paragraph" */),
   Radio: () => import('@primitives/radio/meta' /* webpackChunkName: "Radio" */),
-  LayoutThemeable: () => import('@themeables/layout/meta' /* webpackChunkName: "LayoutThemeable" */),
+  Spacer: () => import('@primitives/spacer/meta' /* webpackChunkName: "Spacer" */),
   TestLottie: () => import('test-lottie/meta' /* webpackChunkName: "TestLottie" */),
   VectorShape: () => import('@primitives/vector-shape/meta' /* webpackChunkName: "VectorShape" */),
 }
