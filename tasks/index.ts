676660685e96a2c3e888c0f8ba4cfbf40e8cd286
Kir Belevich <kir@belevi.ch>
2020-07-01 10:29:27 +0200

    💥 bubble-dev/start-preset: remove X-Ray

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,27 +1,9 @@
 import plugin from '@start/plugin'
-import {
-  CheckChromeScreenshots,
-  CheckFirefoxScreenshots,
-  CheckIosScreenshots,
-  CheckAndroidScreenshots,
-  CheckIosWebScreenshots,
-  CheckAndroidWebScreenshots,
-  Sandbox,
-  CheckChromePerfSnapshots,
-  Pkg,
-} from '@bubble-dev/start-preset'
+import { Sandbox, Pkg } from '@bubble-dev/start-preset'
 
 export * from '@bubble-dev/start-preset'
 
 // custom tasks:
-export const checkChromeScreenshots = CheckChromeScreenshots()
-export const checkFirefoxScreenshots = CheckFirefoxScreenshots()
-export const checkAndroidScreenshots = CheckAndroidScreenshots()
-export const checkIosScreenshots = CheckIosScreenshots()
-export const checkIosWebScreenshots = CheckIosWebScreenshots()
-export const checkAndroidWebScreenshots = CheckAndroidWebScreenshots()
-export const checkChromePerfSnapshots = CheckChromePerfSnapshots()
-
 export const sandbox = Sandbox({
   entryPointPath: './tasks/sandbox/index.tsx',
   htmlTemplatePath: './tasks/sandbox/templates/dev.html',
