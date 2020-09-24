f531716fe47fc6f6528379a9637cd51954bd7826
irmina.kaczmarek <irmina.kaczmarek@klarna.com>
2020-09-24 13:28:45 +0200

    🛠 add components to Sandbox

--- tasks/sandbox/components/index.native.ts
+++ tasks/sandbox/components/index.native.ts
@@ -5,12 +5,15 @@ import * as Checkbox from '@primitives/checkbox/meta'
 import * as Heading from '@primitives/heading/meta'
 import * as Input from '@primitives/input/meta'
 import * as LayoutThemeable from '@themeables/layout/meta'
-import * as TextAlignThemeable from '@themeables/text-align/meta'
+import * as OverideDemo from '@themeables/core/meta'
+import * as OverideDemo2 from '@themeables/core2/meta'
 import * as Paragraph from '@primitives/paragraph/meta'
-import * as Svg from '@primitives/svg/meta'
 import * as Radio from '@primitives/radio/meta'
 import * as Spacer from '@primitives/spacer/meta'
+import * as Svg from '@primitives/svg/meta'
 import * as TestLottie from 'test-lottie/meta'
+import * as TextAlignThemeable from '@themeables/text-align/meta'
+import * as ThemeableBackground2 from '@themeables/background2/meta'
 import * as VectorShape from '@primitives/vector-shape/meta'
 
 export const components: TComponents = {
@@ -19,11 +22,14 @@ export const components: TComponents = {
   Heading: () => Promise.resolve(Heading),
   Input: () => Promise.resolve(Input),
   LayoutThemeable: () => Promise.resolve(LayoutThemeable),
-  TextAlignThemeable: () => Promise.resolve(TextAlignThemeable),
+  OverideDemo2: () => Promise.resolve(OverideDemo2),
+  OverideDemo: () => Promise.resolve(OverideDemo),
   Paragraph: () => Promise.resolve(Paragraph),
-  VectorShape: () => Promise.resolve(VectorShape),
-  Svg: () => Promise.resolve(Svg),
-  TestLottie: () => Promise.resolve(TestLottie),
   Radio: () => Promise.resolve(Radio),
   Spacer: () => Promise.resolve(Spacer),
+  Svg: () => Promise.resolve(Svg),
+  TestLottie: () => Promise.resolve(TestLottie),
+  TextAlignThemeable: () => Promise.resolve(TextAlignThemeable),
+  ThemeableBackground2: () => Promise.resolve(ThemeableBackground2),
+  VectorShape: () => Promise.resolve(VectorShape),
 }
