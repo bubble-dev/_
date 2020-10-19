185c6d0fc99f3f5efeb7d1036ea4e31350a03d69
irmina.kaczmarek <irmina.kaczmarek@klarna.com>
2020-10-19 16:44:25 +0200

    💥 themeables/text-align: rewrite @themeables/text-aling API

--- packages/themeables/text-align/src/index.ts
+++ packages/themeables/text-align/src/index.ts
@@ -1,14 +1,10 @@
 import { setupTheme } from '@themeables/core'
-import type { TThemeables, TOverrideContext } from '@themeables/core'
 import type { TThemeableTextAlign } from './types'
 
 export type { TAlign, TDirection, TThemeTextAlign, TThemeableTextAlign, TThemeableTextAligns } from './types'
 
-export const setupTextAlignTheme = <ComponentMappings>(
-  defaultTheme: TThemeables<TThemeableTextAlign, ComponentMappings>,
-  overrideTheme?: TOverrideContext<TThemeableTextAlign, ComponentMappings>
-) => {
-  const { ThemePiece, createThemeable } = setupTheme<TThemeableTextAlign, ComponentMappings>(defaultTheme, overrideTheme)
+export const setupTextAlignTheme = (overrideTheme = {}) => {
+  const { ThemePiece, createThemeable } = setupTheme<TThemeableTextAlign>(overrideTheme)
 
   return {
     TextAlignTheme: ThemePiece,
