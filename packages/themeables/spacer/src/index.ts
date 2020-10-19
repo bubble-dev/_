c21ea03578abed45d0933cd376963dbb2ed5cced
irmina.kaczmarek <irmina.kaczmarek@klarna.com>
2020-10-19 16:40:35 +0200

    💥 themeables/spacer: rewrite @themeables/spacer API

--- packages/themeables/spacer/src/index.ts
+++ packages/themeables/spacer/src/index.ts
@@ -1,14 +1,10 @@
 import { setupTheme } from '@themeables/core'
-import type { TThemeables, TOverrideContext } from '@themeables/core'
 import type { TThemeableSpacer } from './types'
 
 export type { TThemeSpacer, TThemeableSpacer, TThemeableSpacers } from './types'
 
-export const setupSpacerTheme = <ComponentMappings>(
-  defaultTheme: TThemeables<TThemeableSpacer, ComponentMappings>,
-  overrideTheme?: TOverrideContext<TThemeableSpacer, ComponentMappings>
-) => {
-  const { ThemePiece, createThemeable } = setupTheme<TThemeableSpacer, ComponentMappings>(defaultTheme, overrideTheme)
+export const setupSpacerTheme = (overrideTheme = {}) => {
+  const { ThemePiece, createThemeable } = setupTheme<TThemeableSpacer>(overrideTheme)
 
   return {
     SpacerTheme: ThemePiece,
