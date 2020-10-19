4e7f1df2d3d889f12405a4945ed1c44f2f89d3f8
irmina.kaczmarek <irmina.kaczmarek@klarna.com>
2020-10-19 15:59:14 +0200

    💥 themeables/image: rewrite @themeables/image API

--- packages/themeables/image/src/index.ts
+++ packages/themeables/image/src/index.ts
@@ -1,14 +1,10 @@
 import { setupTheme } from '@themeables/core'
-import type { TThemeables, TOverrideContext } from '@themeables/core'
 import type { TThemeableImage } from './types'
 
 export type { TThemeImage, TThemeableImage, TThemeableImages } from './types'
 
-export const setupImageTheme = <ComponentMappings>(
-  defaultTheme: TThemeables<TThemeableImage, ComponentMappings>,
-  overrideTheme?: TOverrideContext<TThemeableImage, ComponentMappings>
-) => {
-  const { ThemePiece, createThemeable } = setupTheme<TThemeableImage, ComponentMappings>(defaultTheme, overrideTheme)
+export const setupImageTheme = (overrideTheme = {}) => {
+  const { ThemePiece, createThemeable } = setupTheme<TThemeableImage>(overrideTheme)
 
   return {
     ImageTheme: ThemePiece,
