55d5720a9ca3571981015da9f1b3ac164be11daa
dongideon <Gideon.gur@klarna.com>
2021-01-07 17:44:43 +0100

    🌱 primitives/list: style

--- packages/primitives/list/src/List.tsx
+++ packages/primitives/list/src/List.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import { component, startWithType } from 'refun'
+import { component, startWithType, mapWithProps } from 'refun'
 import type { TList } from './types'
 
 export const List = component(
-  startWithType<TList>()
-)(({ id, children }) => (
-  <ul id={id}>{children}</ul>
+  startWithType<TList>(),
+  mapWithProps(() => {
+    const style = {
+      listStyleType: 'none',
+      margin: 0,
+      padding: 0,
+    }
+
+    return {
+      style,
+    }
+  })
+)(({ id, children, style }) => (
+  <ul id={id} style={style}>{children}</ul>
 ))
 
 List.displayName = 'List'
