3e1893327d45dcee26cfa778d0d7f34c6b94ed6c
dongideon <Gideon.gur@klarna.com>
2021-01-11 16:16:08 +0100

    🐞 primitives/list: list native works on sandbox

--- packages/primitives/list/listItemMeta.tsx
+++ packages/primitives/list/listItemMeta.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
+import { Text } from '@primitives/text'
 import type { TComponentConfig } from 'autoprops'
 import type { TListItem } from './src/types'
 
 export const config: TComponentConfig<TListItem> = {
   props: {
     id: ['listItem'],
-    children: ['I am a listItem'],
+    children: [<Text key="first">I am a listItem</Text>],
   },
 }
 
