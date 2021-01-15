edc3ac50368052707feee351d3b0d1ba2e8634fe
dongideon <Gideon.gur@klarna.com>
2021-01-15 13:20:09 +0100

    🐞 primitives/list: fixdeps dosent play well with more than one meta file

--- packages/primitives/list/meta.tsx
+++ packages/primitives/list/meta.tsx
@@ -1,14 +1,14 @@
+import React from 'react'
+import { Text } from '@primitives/text'
 import type { TComponentConfig } from 'autoprops'
-import * as List_ItemsMeta from './listItemMeta'
+import { ListItem } from './src'
 import type { TList } from './src'
 
 export const config: TComponentConfig<TList, 'items'> = {
   props: {
     id: ['list'],
     type: ['orderedList', 'unorderedList'],
-  },
-  children: {
-    items: List_ItemsMeta,
+    children: [<ListItem key="first"><Text>I am a listItem</Text></ListItem>],
   },
   required: ['items'],
 }
