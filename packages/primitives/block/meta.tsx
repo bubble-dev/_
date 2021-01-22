863adf6b152781a48e6efb1ddb476ac4297cde5f
dongideon <Gideon.gur@klarna.com>
2021-01-22 18:06:36 +0100

    🐞 primitives/block: block have meta file

--- packages/primitives/block/meta.tsx
+++ packages/primitives/block/meta.tsx
@@ -3,13 +3,13 @@ import type { TComponentConfig } from 'autoprops'
 import { Text } from '@primitives/text'
 import type { TBlock } from './src'
 
-export const config: TComponentConfig<TBlock, 'items'> = {
+export const config: TComponentConfig<TBlock> = {
   props: {
-    style: [{ minWidth: 100, minHeight: 40, backgroundColor: '#ffb3c7' }],
+    style: [{ minWidth: 100, minHeight: 40, backgroundColor: '#fa4c3c' }],
     role: ['main', 'header', 'footer', 'navigation', 'section', 'secondary', 'primary', 'none'],
     children: [<Text key="first">Me... a Block</Text>],
   },
-  required: ['style', 'children'],
+  required: ['style'],
 }
 
 export { Block as Component } from './src'
