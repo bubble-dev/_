b91dd70a1ec6dc94cf278b70792a2a5a6bdde77f
Atila Fassina <atila@fassina.eu>
2021-01-29 15:14:59 +0100

    🛠 no need for r11y on the primitives

--- packages/primitives/shared/test-utils.ts
+++ packages/primitives/shared/test-utils.ts
@@ -1,5 +1,4 @@
 import { getAppPage } from 'foreal'
-// import { getA11yData } from 'r11y'
 
 export async function renderApp(fixturePath: string) {
   try {
@@ -10,16 +9,3 @@ export async function renderApp(fixturePath: string) {
     throw new Error(error)
   }
 }
-
-// Waiting for r11y next release
-// issue with axe-rules.json not found
-// https://github.com/nextools/metarepo/issues/233
-// export async function getA11yReport(fixturePath: string) {
-//   try {
-//     return await getA11yData({
-//       entryPointPath: fixturePath,
-//     })
-//   } catch (error) {
-//     throw new Error(error)
-//   }
-// }
